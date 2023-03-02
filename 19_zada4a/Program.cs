3f23e25bc64a63770e8c0281fbf4d2f85839a418
Arlan <earl199529@gmail.com>
2023-03-02 21:53:33 +0600

    3 seminar home work

---
+++ 19_zada4a/Program.cs
@@ -0,0 +1,25 @@
+﻿// 19 задача. проверка на палиндром
+
+int Input(string number)
+{
+    Console.Write(number + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int a = 0;
+int b = 0;
+int sum = 0;
+int number;
+a = Input("введите пятнизначное число: ");
+number = a;
+while (a > 0)
+{
+    b = a % 10;
+    sum = (sum * 10) + b;
+    a = a / 10;
+}
+if (number == sum)
+    Console.WriteLine("da");
+else
+    Console.WriteLine("no!");
---
+++ 23_zada4a/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Input(string a)
+{
+    Console.Write(a);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double a = 0;
+double i = 1;
+a = Input("Введите a: ");
+
+while (i <= a)
+{
+    Console.WriteLine(Math.Pow(i, 3));
+    i++;
+}
